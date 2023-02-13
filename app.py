df7a95cba860609481ccd8e7a61faabad325822e
siya <del.siyamittal@gmail.com>
2023-02-13 09:48:08 +0530

    made a class and added funcs for playing music videos using selenium

--- app.py
+++ app.py
@@ -1,115 +1,119 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
-#
-def fetch_poster(movie):
-    movie_index = movies[movies['original_title'] == movie].index[0]
-    return movies['poster_path'][movie_index]
+import base64
+from selenium import webdriver
 
-def recommend(movie):
-    movie_index = movies[movies['original_title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+BACKGROUND = 'heart_bg.jpg'
 
-    recommended_movies=[]
-    recommended_movies_posters = []
-    for i in movies_list:
-        # movie_id=movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].original_title)
-        # recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies  #,recommended_movies_posters
+class MovieRecommender:
 
-def findsimilar(name1,name2):
-    movie_index = movies[movies['original_title'] == name1].index[0]
-    movie_index2 = movies[movies['original_title'] == name2].index[0]
-    return similarity[movie_index][movie_index2]
+    def __init__(self) -> None:
+        self.movies_dict=pickle.load(open('movies_dict.pkl','rb'))
+        self.movies=pd.DataFrame(self.movies_dict)
+        self.poster_paths = pickle.load(open('poster_paths.pkl','rb'))
 
+        self.similarity=pickle.load(open('similarity.pkl','rb'))
+        
 
-movies_dict=pickle.load(open('movies_dict.pkl','rb'))
-movies=pd.DataFrame(movies_dict)
+    def fetch_poster(self, movie):
+        movie_index = self.movies[self.movies['original_title'] == movie].index[0]
+        return self.movies['poster_path'][movie_index]
 
-similarity=pickle.load(open('similarity.pkl','rb'))
+    def recommend(self, movie):
+        movie_index = self.movies[self.movies['original_title'] == movie].index[0]
+        distances = self.similarity[movie_index]
+        movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[0:200]
 
+        recommended_movies=[]
+        for i in movies_list:
+            recommended_movies.append(self.movies.iloc[i[0]].original_title)
+        return recommended_movies  
 
-st.title('Movie recommender system')
+    def findsimilar(self, name1,name2):
+        movie_index = self.movies[self.movies['original_title'] == name1].index[0]
+        movie_index2 = self.movies[self.movies['original_title'] == name2].index[0]
+        return self.similarity[movie_index][movie_index2]
 
-selected_movie_name = st.selectbox(
-    'Select a movie that you liked',
-    movies['original_title'].values)
+    def find_overlap(self, rec1, rec2):
+        for movie1 in rec1:
+            for movie2 in rec2:
+                if movie1 == movie2:
+                    return movie1
+        return "Meri Sapnon Ki Rani kab ayegi tu"
 
-selected_movie_name2 = st.selectbox(
-    'Select a movie that you liked',
-    movies['original_title'].values,key=2)
+    def add_bg(self, image_file):
+        with open(image_file, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+        )  
 
+    def find_music_video(self, movie):
+        driver = webdriver.Firefox()
+        extension = "C:/Users/delsi/AppData/Roaming/Mozilla/Firefox/Profiles/db6rpts6.default-release/extensions/adblockultimate@adblockultimate.net.xpi"
 
-ans=findsimilar(selected_movie_name,selected_movie_name2)
+        driver.install_addon(extension)
+        search = movie + " song"
 
-#including the song
+        driver.get(f"https://www.youtube.com/results?search_query={search}")
+        #video_links = driver.find_elements('xpath','//*[@id="video-title"]')
+        #video_link = video_links[0].get_attribute('href')
+        video_links = driver.find_element('xpath', '//*[@class="style-scope ytd-video-renderer"]')
+        #driver.get(video_links[0].get_attribute('href'))
+        video_links.click()
+        play = driver.find_element('xpath', '//*[@class="ytp-play-button ytp-button"]')
+        play.click()
 
-# this one is using spotify
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-# Authenticate with Spotify
-# auth = SpotifyOAuth(client_id='b4f22aa81b18415b935e04cb57efb948',
-#                      client_secret='a8f11d194b4b4797b418a1cf08c4d86e',
-#                      redirect_uri='http://localhost:8501/callback/')
-# sp = spotipy.Spotify(auth_manager=auth)
-#
-# # Search for the movie and retrieve its soundtrack
-# movie_name = "The Dark Knight"
-# results = sp.search(q=f"{movie_name} soundtrack", type='album')
-# soundtrack = results['albums']['items'][0]
-#
-# # Get the first track from the soundtrack
-# first_track = sp.album_tracks(soundtrack['id'])['items'][0]
-#
-# # Play the first track on Spotify
-# sp.start_playback(uris=[first_track['uri']])
-
-
-
-
-
-if st.button('find our similarity'):
-    st.subheader(ans*10*9/10)
-
-    # names = recommend(selected_movie_name)
-    # col1, col2, col3,col4,col5 = st.columns(5,gap="small")
-    #
-    # with col1:
-    #     st.subheader(names[0])
-    #     st.image(fetch_poster(names[0]))
-    #
-    # with col2:
-    #     st.subheader(names[1])
-    #     st.image(fetch_poster(names[1]))
-    #
-    # with col3:
-    #     st.subheader(names[2])
-    #     st.image(fetch_poster(names[2]))
-    #
-    # with col4:
-    #     st.subheader(names[3])
-    #     st.image(fetch_poster(names[3]))
-    #
-    # with col5:
-    #     st.subheader(names[4])
-    #     st.image(fetch_poster(names[4]))
+        #st.video(video_links[0].get_attribute('href'), start_time=0)
 
 
 
-# to play using youtube
+if __name__ == '__main__':
+    mv = MovieRecommender()
+
+    mv.add_bg(BACKGROUND) 
+
+    st.title('Movie Recommender')
+
+    selected_movie_name1 = st.selectbox(
+        'Select a movie that you like',
+        mv.movies['original_title'].values)
+
+    selected_movie_name2 = st.selectbox(
+        'Select a movie that you like',
+        mv.movies['original_title'].values,key=2)
+
+
+    if st.button('Find our similarity'):
+        ans = mv.findsimilar(selected_movie_name1,selected_movie_name2)
+        st.subheader(ans*10*9/10)
+        recommendations1 = mv.recommend(selected_movie_name1)
+        recommendations2 = mv.recommend(selected_movie_name2)
+
+        print(recommendations1)
+        print(recommendations2)
+        
+        overlap = mv.find_overlap(recommendations1, recommendations2)   
+
+        if ans < 0.3:
+            st.subheader(f"You have different tastes but why not try {overlap} ?")
+        else:
+            st.subheader(f"The Movie That Connects You is : {overlap}")
+            
+        print(mv.poster_paths)
+        st.image(mv.poster_paths[overlap])
+        mv.find_music_video(overlap)
+        print(mv.poster_paths["Uri: The Surgical Strike"])
+
+
     
-# import os
-# import webbrowser
-# 
-# def play_song_on_youtube(movie_title):
-#     search_query = movie_title + ' soundtrack'
-#     webbrowser.open('https://www.youtube.com/results?search_query=' + search_query)
-# 
-# movie_title = 'The Shawshank Redemption'
-# 
-# play_song_on_youtube(movie_title)
 
